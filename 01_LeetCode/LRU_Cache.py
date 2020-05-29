d9135117c7642ce558ff48bffdfb29d2e54f85b8
trixxupthesleeave <kaushik.bhimraj@gmail.com>
2020-05-29 01:15:58 -0400

    LRU Cache, still in progress.

---
+++ 01_LeetCode/LRU_Cache.py
@@ -0,0 +1,76 @@
+"""
+Design and implement a data structure for Least Recently Used (LRU) cache. 
+It should support the following operations: get and put. 
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, 
+otherwise return -1
+
+put(key, value) - Set or insert the value if the key is not already present. When the cache 
+reached its capacity, it should invalidate the least recently used item before inserting a 
+new item. 
+
+The cache is initialized with a positive capacity. 
+
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+	LRUCache cache = new LRUCache( 2 /* capacity */ );
+
+	cache.put(1, 1);
+	cache.put(2, 2);
+	cache.get(1);       // returns 1
+	cache.put(3, 3);    // evicts key 2
+	cache.get(2);       // returns -1 (not found)
+	cache.put(4, 4);    // evicts key 1
+	cache.get(1);       // returns -1 (not found)
+	cache.get(3);       // returns 3
+	cache.get(4);       // returns 4
+"""
+ 
+class LRUCache:
+
+	def __init__(self, capacity: int):
+		self.size  = capacity - 1
+		self.queue = []
+		self.cache = {}
+
+	def get(self, key: int) -> int:
+		try:
+			currValue = self.queue.pop(key)
+			self.queue = [currValue] + self.queue
+			return self.cache[key]
+		except KeyError:
+			return -1
+
+	def put(self, key: int, value: int) -> None:
+		
+		if len(self.queue) > self.size:
+			self.cache.pop(self.queue.pop(-1))
+			
+			self.queue = [value] + self.queue
+			self.cache[key] = value
+		
+		else:
+			self.cache[key] = value
+			self.queue = [value] + self.queue
+
+
+x = LRUCache(2)
+x.put(1,1)
+x.put(2,2)
+
+print(x.queue)
+
+print(x.get(1))
+
+x.put(3,3)
+
+print(x.get(2))
+
+x.put(4,4)
+
+print(x.get(1))
+print(x.get(3))
+print(x.get(4))
