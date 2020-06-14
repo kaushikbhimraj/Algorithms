cc48bfaf32d1ecdb905b8407897287abfe78d263
trixxupthesleeave <kaushik.bhimraj@gmail.com>
2020-06-13 23:25:24 -0400

    Figured out 3Sum and the logic behind it.

--- 08_Alg_Exp/BST_Construction.py
+++ 08_Alg_Exp/BST_Construction.py
@@ -20,16 +20,113 @@ class BST:
         self.left = None
         self.right = None
 
+
     def insert(self, value):
         # Write your code here.
         # Do not edit the return statement of this method.
+        currentNode = self
+        while True:
+
+        	# Check if value is greater/equal or less than the value in current node. 
+        	# If you reach a leaf, insert the value in as the child of the node. 
+
+        	# RIGHT
+        	if currentNode.value <= value:
+        		if not currentNode.right:
+        			currentNode.right = BST(value)
+        			break
+        		else:
+        			currentNode = currentNode.right
+
+        	# LEFT
+        	else:
+        		if not currentNode.left:
+        			currentNode.left = BST(value)
+        			break
+        		else:
+        			currentNode = currentNode.left
+
+        # Return the head node. 
         return self
+
 
     def contains(self, value):
         # Write your code here.
-        pass
+        currentNode	= self
 
-    def remove(self, value):
+        # First check value match the node value or else traverse left or right. 
+        while currentNode:
+        	if currentNode.value == value:
+        		return True
+
+        	if currentNode.value < value:
+        		currentNode = currentNode.right
+        	else:
+        		currentNode = currentNode.left
+        return False
+
+
+    def remove(self, value, parentNode=None):
         # Write your code here.
         # Do not edit the return statement of this method.
+        currentNode = self
+
+        # Tree main edge cases 
+        # 	1. two children are not None
+        #	2. node is root node
+        # 	3. has one child 
+
+        while currentNode:
+    
+        	# Traversing BST until reaching node. 
+        	# Storing the previous node as its parent node. PRETTY NEAT TRICK!!
+        	if currentNode.value < value:
+        		parentNode = currentNode
+        		currentNode = currentNode.right
+
+        	elif currentNode.value > value:
+        		parentNode = currentNode
+        		currentNode = currentNode.left
+    		
+    		# Check above three edge cases after reaching the deletion node. 
+        	else:
+        		# Case 1:
+        		if currentNode.left and currentNode.right:
+        			currentNode.value = currentNode.right.getMinValue()
+        			currentNode.right.remove(currentNode.value, currentNode)
+
+        		# Case 2:
+        		# If the above sequence did not run, then the following code with see if the node is root. 
+        		# Also will also check if which of the children is NOT NONE 
+        		# And manually the node over the main node.
+	        	elif not parentNode:
+	        		if currentNode.left:
+	        			currentNode.value = currentNode.left.value
+	        			currentNode.right = currentNode.left.right
+	        			currentNode.left  = currentNode.left.left
+
+	        		elif currentNode.right:
+	        			currentNode.value = currentNode.right.value
+	        			currentNode.left  = currentNode.right.left
+	        			currentNode.right = currentNode.right.right
+
+	        		# If the root node does not have any children, then skip.
+	        		else:
+	        			pass
+
+        		# Case 3:
+        		# This case catches the sequence where there is atleast on 
+	        	elif parentNode.left == currentNode:
+	        		parentNode.left = currentNode.left if currentNode.left else currentNode.right
+	        	elif parentNode.right == currentNode:
+	        		parentNode.right = currentNode.left if currentNode.left else currentNode.right
+	        	break
+
         return self
+
+    # Helper function to the left most node in the right sub-tree for a node. 
+    def getMinValue(self):
+    	currentNode = self
+    	while currentNode.left:
+    		currentNode = currentNode.left
+    	return currentNode.value
