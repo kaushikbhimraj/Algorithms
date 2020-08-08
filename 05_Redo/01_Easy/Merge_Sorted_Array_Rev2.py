a78f4beb4b0a000869d8de36d7bca6fd51e3021e
Kaushik Bhimraj <kaushik.bhimraj@gmail.com>
2020-08-08 01:31:53 -0400

    07.08.2020

--- 05_Redo/01_Easy/Merge_Sorted_Array_Rev2.py
+++ 05_Redo/01_Easy/Merge_Sorted_Array_Rev2.py
@@ -24,10 +24,5 @@ nums2.length == n
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
         # Simply way of solving this. (Using the sort function of Python)
-        for i in range(m, len(nums1)):
-            nums1[i] = nums2[i-m]
-        return nums1.sort()
+        nums1[:] = sorted(nums1[m:] + nums2)
