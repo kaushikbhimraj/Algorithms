01ae947397868f3775f28ae01d01083812c5a4a7
Kaushik Bhimraj <kaushik.bhimraj@gmail.com>
2020-10-15 13:02:29 -0400

    10.15.2020

--- 01_LeetCode/03_LeetCode_Curated/02_Medium/Design_Tic_Tac_Toe.py
+++ 01_LeetCode/03_LeetCode_Curated/02_Medium/Design_Tic_Tac_Toe.py
@@ -1,11 +1,85 @@
+"""
+Assume the following rules are for the tic-tac-toe game on an n x n board between two players:
+
+A move is guaranteed to be valid and is placed on an empty block.
+Once a winning condition is reached, no more moves are allowed.
+A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+Implement the TicTacToe class:
+
+TicTacToe(int n) Initializes the object the size of the board n.
+int move(int row, int col, int player) Indicates that player with id player plays at the cell (row, col) of 
+the board. The move is guaranteed to be a valid move.
+
+Follow up:
+Could you do better than O(n2) per move() operation?
+
+Example 1:
+
+Input
+["TicTacToe", "move", "move", "move", "move", "move", "move", "move"]
+[[3], [0, 0, 1], [0, 2, 2], [2, 2, 1], [1, 1, 2], [2, 0, 1], [1, 0, 2], [2, 1, 1]]
+Output
+[null, 0, 0, 0, 0, 0, 0, 1]
+
+Explanation
+TicTacToe ticTacToe = new TicTacToe(3);
+Assume that player 1 is "X" and player 2 is "O" in the board.
+ticTacToe.move(0, 0, 1); // return 0 (no one wins)
+|X| | |
+| | | |    // Player 1 makes a move at (0, 0).
+| | | |
+
+ticTacToe.move(0, 2, 2); // return 0 (no one wins)
+|X| |O|
+| | | |    // Player 2 makes a move at (0, 2).
+| | | |
+
+ticTacToe.move(2, 2, 1); // return 0 (no one wins)
+|X| |O|
+| | | |    // Player 1 makes a move at (2, 2).
+| | |X|
+
+ticTacToe.move(1, 1, 2); // return 0 (no one wins)
+|X| |O|
+| |O| |    // Player 2 makes a move at (1, 1).
+| | |X|
+
+ticTacToe.move(2, 0, 1); // return 0 (no one wins)
+|X| |O|
+| |O| |    // Player 1 makes a move at (2, 0).
+|X| |X|
+
+ticTacToe.move(1, 0, 2); // return 0 (no one wins)
+|X| |O|
+|O|O| |    // Player 2 makes a move at (1, 0).
+|X| |X|
+
+ticTacToe.move(2, 1, 1); // return 1 (player 1 wins)
+|X| |O|
+|O|O| |    // Player 1 makes a move at (2, 1).
+|X|X|X|
+ 
+Constraints:
+
+2 <= n <= 100
+player is 1 or 2.
+1 <= row, col <= n
+(row, col) are unique for each different call to move.
+At most n2 calls will be made to move.
+"""
+
+# https://leetcode.com/problems/design-tic-tac-toe/discuss/81898/Java-O(1)-solution-easy-to-understand
+# Time: O(1)
+# Space: O(n^2)
+
 class TicTacToe:
 
     def __init__(self, n: int):
         """
         Initialize your data structure here.
         """
-
-        # Create four counters. These counters are little different from basic counters. 
+        # Create four counters. 
+        # These counters are little different from basic counters. 
         self.row = [0] * n
         self.col = [0] * n
         self.diag = 0
@@ -42,7 +116,7 @@ class TicTacToe:
         # If any of these four counters reach n size, then you have a winner!
         if abs(self.row[row]) == self.n or abs(self.col[col]) == self.n or abs(self.diag) == self.n or abs(self.adiag) == self.n:
             return player
-            
+
         return 0
         
 
