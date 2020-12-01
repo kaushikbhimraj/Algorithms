43a32549f51b5c262d5b95459a1a8c705dfed494
Kaushik B <kaushik.bhimraj@gmail.com>
2020-11-30 19:17:50 -0500

    Update Graph_Cycle.py

--- 04_Algorithms_Data_Structures/Graph_Cycle.py
+++ 04_Algorithms_Data_Structures/Graph_Cycle.py
@@ -3,58 +3,60 @@ Given an undirected graph, check whether the graph contains a cycle or not.
 Your function should return true if the given graph contains at least one 
 cycle, else return false.
 """
-
-# Node in graph. 
-class Vertex:
+# Standard graph node (undirected)
+class Node:
 	def __init__(self, val):
 		self.val = val
 		self.adj = []
 
-# Basic graph object.
+# Standard Graph
 class Graph:
-	def __init__(self, val):
+	def __init__(self):
 		self.vertics = {}
 
 	def addNode(self, val):
-		if val not in self.vertics.keys():
-			self.vertics[val] = Vertex(val)
+		if val not in self.vertics:
+			self.vertics[val] = Node(val)
 			return True
 		else:
 			return False
 
-	def addEdge(self, g1, g2):
-		if g1.val not in self.vertics.keys() or g2.val not in self.vertics.keys():
+	def addEdge(self, a, b):
+		if a.val not in self.vertics or b.val not in self.vertics:
 			return False
-		g1.adj.append(g2)
-		g2.adj.append(g1)
+
+		a.adj.append(b)
+		b.adj.append(a)
 		return True
 
-# Union-Find
+# Dis-joint creates a set for each vertex in graph and 
+# uses find method to link values to a set. 
 class disJointSet:
 	def __init__(self):
 		self.parent = {}
 
-	def makeSet(self, vertics):
-		self.parent = {vertex:vertex for vertex in vertics}
+	def makeSet(self, g):
+		self.parent = {x:x for x in g.vertics}
 
 	def find(self, x):
-		if self.parent[x] == x:
-			return x
-		self.find(self.parent[x])
+		if (self.parent[x] == x):
+			return self.parent[x]
+		else:
+			self.find(self.parent[x])
 
 	def union(self, a, b):
-		self.parent[a] = b
+		self.parent[x] = y
 
-# Union-Find Cycle Detection
+# Using union-find, detect cycles in graph.
 def findCycle(graph, n):
 	ds = disJointSet()
-	ds.makeSet(graph.vertics.keys())
-	for vertex in graph.vertics.keys():
-		for neighbor in graph.vertics[vertex].adj:
+	ds.makeSet(graph)
+	for vertex in graph.vertics.values():
+		for neighbor in vertex.adj:
 			x = ds.find(vertex.val)
 			y = ds.find(neighbor.val)
-			if x != y:
-				ds.union(x, y)
+			if (x != y):
+				ds.union(x,y)
 			else:
 				return True
 	return False
