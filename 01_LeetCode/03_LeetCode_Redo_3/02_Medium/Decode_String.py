60fa1b3876d0c36c83d9b3f0decb41937c061879
trixxupthesleeave <kaushik.bhimraj@gmail.com>
2021-01-04 00:13:12 -0500

    01.04.2020

--- 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Decode_String.py
+++ 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Decode_String.py
@@ -1,63 +1,68 @@
-class Solution:
-    def decodeString(self, s: str) -> str:
-        if (len(s) == 1):
-            return s
-        # If string contains nested brackets, best to use stacks. 
-        stack = []
-        res = ""
-        
-        # T: O(n); S: O(n)
-        for i in range(len(s)):
-            if (s[i] == "]" or s[i] == "["):
-                continue
-            else:
-                stack.append(s[i])
-                
-        # T: O(n);
-        i = len(stack)
-        val = stack.pop()
-        while stack:
-            num = ""
-            in_string = ""
-            
-            while (stack and not val.isdigit()):
-                in_string = val + in_string
-                val = stack.pop()
-            
-            while (stack and val.isdigit()):
-                num = val + num
-                val = stack.pop()
-                
-            if num:
-                in_string = in_string * int(num)
-                res = in_string + res
-            else:
-                continue
-        
-        # Messy!
-        if (val.isdigit()):
-            in_string = in_string * int(val)
-        else:
-            in_string = val + in_string
-        res = in_string + res
-        
-        return res
-
 """
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets 
+is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; No extra white spaces, square brackets 
+are well-formed, etc.
+
+Furthermore, you may assume that the original data does not contain any digits and that digits 
+are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+Example 1:
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+
+Example 2:
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+
+Example 3:
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+
+Example 4:
+Input: s = "abc3[cd]xyz"
+Output: "abccdcdcdxyz"
+ 
+Constraints:
+1 <= s.length <= 30
+s consists of lowercase English letters, digits, and square brackets '[]'.
+s is guaranteed to be a valid input.
+All the integers in s are in the range [1, 300].
+"""
+
+# T: O((max(number)^(number of integers in string)) * )
 class Solution:
-    def decodeString(self, s: str) -> str:
-        stack = []
+    def decodeString0(self, s: str) -> str:
+        # Use stack to pull substring. 
+        # When you hit ], start popping from stack and build a string until you hit [ on stack.
+        # Then pop [ from stack and extract number. 
+        # Create in_string by multiplying the substring with number. 
+        # Put the string back on the stack. 
         
+        # When you hit another ], you start popping values again. 
+        # Until end of the string. 
+        stack = []
         for i in range(len(s)):
             if s[i] == "]":
-                val = stack.pop()
-                inString = ""
                 
-                while (stack and val != "["):
-                    inString = inString + val
-                    val = stack.pop()
+                in_string = ""
+                while (stack and stack[-1] != "["):
+                    in_string = stack.pop() + in_string
                 
-                print(inString)
+                stack.pop()
+                num = ""
+                while (stack and stack[-1].isdigit()):
+                    num = stack.pop() + num
+                in_string = in_string * int(num)
+                
+                # MOST IMPORTANT STEP IN LOGIC!!!
+                for char in in_string:
+                    stack.append(char)
             else:
                 stack.append(s[i])
-"""
+        
+        # Put everthing back together and return
+        return "".join(stack)
