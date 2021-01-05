e650a8346b1e136f6b222eb2cdffc1836e6d1aab
trixxupthesleeave <kaushik.bhimraj@gmail.com>
2021-01-04 21:57:37 -0500

    12.04.2020

--- 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Minimum_Remove_to_Make_Valid_Parentheses.py
+++ 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -1,4 +1,3 @@
-
 """
 Given a string of '(', ')' and lowercase English characters. 
 Your task is to remove the minimum number of parenthesis ('(' or ')') in any positions )
@@ -6,6 +5,28 @@ so the resutling parenthesis string is valid if and only if:
 	- It is the empty string, contains only lowercase characters, or
 	- It can be written as AB (A concatenated with B), where A and B are valid strings, or
 	- It can be written as (A), where A is a valid string. 
+
+Example 1:
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+
+Example 2:
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+
+Example 3:
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+
+Example 4:
+Input: s = "(a(b(c)d)"
+Output: "a(b(c)d)"
+
+Constraints:
+1 <= s.length <= 10^5
+s[i] is one of  '(' , ')' and lowercase English letters.
 """
 
 class Solution:
