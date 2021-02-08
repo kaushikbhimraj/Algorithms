ca66926a20054944ef48068da9e6a53d6025bc87
trixxupthesleeave <kaushik.bhimraj@gmail.com>
2021-02-07 21:19:24 -0500

    02.07.2021

---
+++ 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Interval_List_Intersections.py
@@ -0,0 +1,54 @@
+"""
+You are given two lists of closed intervals, firstList and secondList, where firstList[i] = 
+[starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
+
+Return the intersection of these two interval lists.
+
+A closed interval [a, b] (with a < b) denotes the set of real numbers x with a <= x <= b.
+
+The intersection of two closed intervals is a set of real numbers that are either empty or represented 
+as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+
+ 
+
+Example 1:
+Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+
+Example 2:
+Input: firstList = [[1,3],[5,9]], secondList = []
+Output: []
+
+Example 3:
+Input: firstList = [], secondList = [[4,8],[10,12]]
+Output: []
+
+Example 4:
+Input: firstList = [[1,7]], secondList = [[3,10]]
+Output: [[3,7]]
+
+T: O(n); S: O(n) where n is max(len(firstList), len(secondList))
+Video - https://www.youtube.com/watch?v=Qh8ZjL1RpLI
+"""
+
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        if not firstList or not secondList:
+            return []
+        
+        # Create two pointers
+        # Make sure they don't exceed min(len(firstList, secondList))
+        l1, l2 = len(firstList), len(secondList)
+        one, two = 0, 0
+        out = []
+        while (one < l1 and two < l2):
+            if (secondList[two][0] <= firstList[one][1] and secondList[two][1] >= firstList[one][0]):
+                x = max(firstList[one][0], secondList[two][0])
+                y = min(firstList[one][1], secondList[two][1])
+                out.append([x,y])
+            
+            if (firstList[one][1] < secondList[two][1]):
+                one += 1
+            else:
+                two += 1
+        return out
