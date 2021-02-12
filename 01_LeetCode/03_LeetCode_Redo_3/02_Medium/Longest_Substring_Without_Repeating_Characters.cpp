4362b846d77245b7c134d714c4c68868436eee02
trixxupthesleeave <kaushik.bhimraj@gmail.com>
2021-02-11 23:05:42 -0500

    02.11.2021

---
+++ 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.length() == 0) return 0;
+        std::map<char, int>locations;
+        std::map<char, int>::iterator val;
+        int i = 0, j = 0;
+        int size = 0;
+
+        while (j < s.length()){
+            char temp = s[j];
+            val = locations.find(temp);
+            if (val != locations.end() && i <= locations[temp]){
+                i = locations[temp] + 1;
+            }
+            locations[temp] = j;
+            int len = j - i + 1;
+            size = (size < len) ? len : size;
+            j++;
+        }
+        return size;
+    }
+};
