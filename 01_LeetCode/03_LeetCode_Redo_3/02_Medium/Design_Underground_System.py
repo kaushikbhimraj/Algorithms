2d05defb7f7a4f3569d66b39936e8b9c153e86d9
trixxupthesleeave <kaushik.bhimraj@gmail.com>
2021-02-13 21:18:17 -0500

    02.13.2021

--- 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Design_Underground_System.py
+++ 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Design_Underground_System.py
@@ -1,104 +1,46 @@
 """
 Implement the UndergroundSystem class:
-void checkIn(int id, string stationName, int t)
-A customer with a card id equal to id, gets in the station stationName at time t.
-A customer can only be checked into one place at a time.
-void checkOut(int id, string stationName, int t)
-A customer with a card id equal to id, gets out from the station stationName at time t.
-double getAverageTime(string startStation, string endStation)
-Returns the average time to travel between the startStation and the endStation.
-The average time is computed from all the previous traveling from startStation to endStation that happened directly.
-Call to getAverageTime is always valid.
-You can assume all calls to checkIn and checkOut methods are consistent. If a customer gets in at time t1 at some 
-station, they get out at time t2 with t2 > t1. All events happen in chronological order.
 
-Example 1:
-Input
-["UndergroundSystem","checkIn","checkIn","checkIn","checkOut","checkOut","checkOut","getAverageTime","getAverageTime",
-"checkIn","getAverageTime","checkOut","getAverageTime"]
-[[],[45,"Leyton",3],[32,"Paradise",8],[27,"Leyton",10],[45,"Waterloo",15],[27,"Waterloo",20],[32,"Cambridge",22],
-["Paradise","Cambridge"],["Leyton","Waterloo"],[10,"Leyton",24],["Leyton","Waterloo"],[10,"Waterloo",38],
-["Leyton","Waterloo"]]
+    void checkIn(int id, string stationName, int t)
+        A customer with a card id equal to id, gets in the station stationName at time t.
+        A customer can only be checked into one place at a time.
+    void checkOut(int id, string stationName, int t)
+        A customer with a card id equal to id, gets out from the station stationName at time t.
+    double getAverageTime(string startStation, string endStation)
+        Returns the average time to travel between the startStation and the endStation.
+        The average time is computed from all the previous traveling from startStation to endStation that happened directly.
+        Call to getAverageTime is always valid.
 
-Output
-[null,null,null,null,null,null,null,14.00000,11.00000,null,11.00000,null,12.00000]
+You can assume all calls to checkIn and checkOut methods are consistent. If a customer 
+gets in at time t1 at some station, they get out at time t2 with t2 > t1. All events happen in chronological order.
 
-Explanation
-UndergroundSystem undergroundSystem = new UndergroundSystem();
-undergroundSystem.checkIn(45, "Leyton", 3);
-undergroundSystem.checkIn(32, "Paradise", 8);
-undergroundSystem.checkIn(27, "Leyton", 10);
-undergroundSystem.checkOut(45, "Waterloo", 15);
-undergroundSystem.checkOut(27, "Waterloo", 20);
-undergroundSystem.checkOut(32, "Cambridge", 22);
-undergroundSystem.getAverageTime("Paradise", "Cambridge");       // return 14.00000. There was only one travel from "Paradise" (at time 8) to "Cambridge" (at time 22)
-undergroundSystem.getAverageTime("Leyton", "Waterloo");          // return 11.00000. There were two travels from "Leyton" to "Waterloo", a customer with id=45 from time=3 to time=15 and a customer with id=27 from time=10 to time=20.
-So the average time is ( (15-3) + (20-10) ) / 2 = 11.00000
-undergroundSystem.checkIn(10, "Leyton", 24);
-undergroundSystem.getAverageTime("Leyton", "Waterloo");          // return 11.00000
-undergroundSystem.checkOut(10, "Waterloo", 38);
-undergroundSystem.getAverageTime("Leyton", "Waterloo");          // return 12.00000
-
-Example 2:
-Input
-["UndergroundSystem","checkIn","checkOut","getAverageTime","checkIn","checkOut","getAverageTime","checkIn","checkOut",
-"getAverageTime"]
-[[],[10,"Leyton",3],[10,"Paradise",8],["Leyton","Paradise"],[5,"Leyton",10],[5,"Paradise",16],["Leyton","Paradise"],
-[2,"Leyton",21],[2,"Paradise",30],["Leyton","Paradise"]]
-
-Output
-[null,null,null,5.00000,null,null,5.50000,null,null,6.66667]
-
-Explanation
-UndergroundSystem undergroundSystem = new UndergroundSystem();
-undergroundSystem.checkIn(10, "Leyton", 3);
-undergroundSystem.checkOut(10, "Paradise", 8);
-undergroundSystem.getAverageTime("Leyton", "Paradise"); // return 5.00000
-undergroundSystem.checkIn(5, "Leyton", 10);
-undergroundSystem.checkOut(5, "Paradise", 16);
-undergroundSystem.getAverageTime("Leyton", "Paradise"); // return 5.50000
-undergroundSystem.checkIn(2, "Leyton", 21);
-undergroundSystem.checkOut(2, "Paradise", 30);
-undergroundSystem.getAverageTime("Leyton", "Paradise"); // return 6.66667
-
-Constraints:
-There will be at most 20000 operations.
-1 <= id, t <= 106
-All strings consist of uppercase and lowercase English letters, and digits.
-1 <= stationName.length <= 10
-Answers within 10-5 of the actual value will be accepted as correct.
+T: O(1); S: P(P + S^2) where P is number of passengers and S is number of stations. We can expect to all possible 
+combinations of stations in the self.stations dictionary we will have S^2 space complexity. 
 """
-
 class UndergroundSystem:
 
-	# Dictionaries to keep track of start and end stations and times for the users. 
-	# Dictionaries to keep track of average times for the routes taken by the users.
     def __init__(self):
         self.users = {}
-        self.routes = {}
+        self.stations = {}
 
-    # Create a new record of the user while checking in. 
-    # If prior data exists, simply overwrite it. 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.users[id] = [stationName, "", t, 0]
+        self.users[id] = [[stationName, t], []]
 
-    # Check outs are always done after a check in.  
-    # Use the self.users map to add the check out station name and time. 
-    # Add the users delta time to the self.routes. 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        self.users[id][1] = stationName
-        self.users[id][3] = t
-        startStation = self.users[id][0]
-        startTime = self.users[id][2]
-        if (startStation, stationName) in self.routes:
-            self.routes[(startStation, stationName)].append([id, t-startTime])
+        self.users[id][1] = [stationName, t]
+        temp = self.users[id][0][0] + stationName
+        if temp in self.stations:
+            self.stations[temp].append(t - self.users[id][0][1])
         else:
-            self.routes[(startStation, stationName)] = [[id, t-startTime]]
+            self.stations[temp] = [t - self.users[id][0][1]]
 
-    # Calculate the average time based on the startStation and endStation routes. 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        avgTimes = self.routes[(startStation, endStation)]
-        res = 0
-        for _, time in avgTimes:
-            res += time
-        return res/len(avgTimes)
+        size = len(self.stations[startStation+endStation])
+        return sum(self.stations[startStation+endStation])/size
+
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)
