afd48acca9586a09841e662f6d3d116240e921b8
trixxupthesleeave <kaushik.bhimraj@gmail.com>
2021-02-15 00:24:45 -0500

    02.14.2021

--- 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Reconstruct_Itinerary.py
+++ 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Reconstruct_Itinerary.py
@@ -16,27 +16,37 @@ Example 1:
 Input: [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
 Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
 """
+
 from heapq import heappush, heappop
 class Solution:
+
+    # Create an adjacency matrix
+    # Values for each key in adj matix hash map will be a heap. 
+    # Use depth first search through adj matrix. 
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        # Build an adjacency map
-        itin = {}
-        for x,y in tickets:
-            if x in itin:
-                heappush(itin[x], y)
-            else:
-                itin[x] = [y]
+        routes = defaultdict(list)
+        for ticket in tickets:
+            heappush(routes[ticket[0]], ticket[1])
+
+        stack = []
+        self.dfs(stack, routes, 'JFK')
         
+        # Since the return on the DFS will be a stack (all values are reverse order)
         res = []
-        self.helper("JFK", itin, res)
+        i = len(stack) - 1
+        while (i >= 0):
+            res.append(stack[i])
+            i -= 1
+
+        # Return populated output. 
         return res
     
-    def helper(self, start, itin, out):
-        if (start not in itin or not itin[start]):
-            out.append(start)
-            return 
-        
-        out.append(start)
-        newStart = heappop(itin[start])
-        return self.helper(newStart, itin, out)
-        
+    # Load the heap for the key(start). 
+    # Check if heap exists and then proceed to pop the top from heap and use it as start for 
+    # the next recursion. Once the while loop is done, add the start value to output array.
+    def dfs(self, stack, routes, start):
+        heap = routes[start]
+        while heap:
+            end = heappop(heap)
+            self.dfs(stack, routes, end)
+        stack.append(start)
