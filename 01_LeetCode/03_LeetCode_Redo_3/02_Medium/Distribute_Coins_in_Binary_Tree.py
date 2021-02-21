ce864565a44c29d9cce35034c225fa10c1a8dd11
trixxupthesleeave <kaushik.bhimraj@gmail.com>
2021-02-20 20:31:18 -0500

    02.20.2021

---
+++ 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Distribute_Coins_in_Binary_Tree.py
@@ -0,0 +1,38 @@
+"""
+You are given the root of a binary tree with n nodes where each node in the tree has node.val 
+coins and there are n coins total.
+
+In one move, we may choose two adjacent nodes and move one coin from one node to another. (A 
+move may be from parent to child, or from child to parent.)
+
+Return the number of moves required to make every node have exactly one coin.
+
+Example 1:
+Input: root = [3,0,0]
+Output: 2
+
+Explanation: From the root of the tree, we move one coin to its left child, and one coin to its right child.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def distributeCoins(self, root: TreeNode) -> int:
+        self.temp = 0
+        self.dfs(root)
+        return self.temp
+    
+    def dfs(self, root):
+        if not root:
+            return 0
+        
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
+        
+        self.temp += abs(left) + abs(right)
+        return root.val + left + right - 1
