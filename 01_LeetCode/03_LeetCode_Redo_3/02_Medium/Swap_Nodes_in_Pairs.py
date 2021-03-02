c44fad52b9bb67b79e7fde652d637221fb7fe242
trixxupthesleeave <kaushik.bhimraj@gmail.com>
2021-03-01 23:46:18 -0500

    03.01.2021

--- 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Swap_Nodes_in_Pairs.py
+++ 01_LeetCode/03_LeetCode_Redo_3/02_Medium/Swap_Nodes_in_Pairs.py
@@ -44,3 +44,24 @@ class Solution:
                 node = node.next
 
         return head
+
+    def swapPairs(self, head: ListNode) -> ListNode:
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        # If the list has no node or has only one node left.
+        if not head or not head.next:
+            return head
+
+        # Nodes to be swapped
+        first_node = head
+        second_node = head.next
+
+        # Swapping
+        first_node.next  = self.swapPairs(second_node.next)
+        second_node.next = first_node
+
+        # Now the head is the second node
+        return second_node        
